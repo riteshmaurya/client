6b0cfd3746d02ab3fe74e8ef99955d8cc786ce01
riteshmaurya@gmail.com <riteshmaurya@gmail.com>
2019-11-13 21:27:04 +1300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { FibComponent } from './fib/fib.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OtherPageComponent } from './other-page/other-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'otherpage',
+    component: OtherPageComponent
+  },
+  {
+    path: '',
+    component: FibComponent
+  },
+  {
+    path: '',
+    redirectTo: '/',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { OtherPageComponent } from './other-page/other-page.component';
+import { FibComponent } from './fib/fib.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OtherPageComponent,
+    FibComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
